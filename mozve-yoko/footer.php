d1af3ed7739f0101899e924eba16b21a68fcd185
ThePhoenixBird <fenix.kun@gmail.com>
2012-08-12 23:17:48 -0430

    Tema hijo de Yoko, modificado y diseñado por Arturo Martinez (ThePhoenixBird) para toda la comunidad de Mozilla Venezuela.

---
+++ mozve-yoko/footer.php
@@ -0,0 +1,35 @@
+﻿<?php
+/**
+ * @package WordPress
+ * @subpackage Yoko
+ */
+?>
+
+</div><!-- end wrap -->
+
+<footer id="site-info" role="contentinfo">
+  <div class="wrap">
+    <p id="foot-logo">
+      <a class="logo" href="http://mozilla.org" rel="external">Mozilla</a>
+    </p>
+    
+    <p id="footer-colophon">A no ser que se indique lo contrario, el contenido de este sitio está licenciado bajo <a href="http://creativecommons.org/licenses/by-sa/3.0/" rel="external">Creative Commons Attribution Share-Alike License v3.0</a> o una versión posterior.</p>
+    
+    <nav id="nav-meta">
+      <ul role="navigation">
+        <li><a href="http://wwww.mozillavenezuela.org/contacto/" rel="external">Contáctanos</a></li>
+        <li><a href="http://www.mozilla-europe.org/es/about/privacy/" rel="external">Política de privacidad</a></li>
+        <li><a href="http://www.mozilla.org/es-ES/about/legal.html" rel="external">Información legal</a></li>
+        <li><a href="http://www.mozilla.org/es-ES/legal/fraud-report/index.html" rel="external">Reportar abuso de marcas</a></li> 
+      </ul>
+    </nav>
+  </div>
+</footer>
+	<script src="//www.mozilla.org/tabzilla/media/js/tabzilla.js"></script>
+</div>
+
+<!-- end page -->
+<?php wp_footer(); ?>
+
+</body>
+</html>
